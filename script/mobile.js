60bc20b480748b35519d0298a73054f30878bc66
Buschi23 <107368011+Buschi23@users.noreply.github.com>
2022-06-23 14:13:59 +0200

    Create mobile.js

---
+++ script/mobile.js
@@ -0,0 +1,9 @@
+$(document).ready(funtion(){
+                  });
+
+$(windows.load(function() {
+  
+  if (screen.width <=699) {
+  document.location= "https://reparaturservice-kormann.de/mobile";
+  }
+});
